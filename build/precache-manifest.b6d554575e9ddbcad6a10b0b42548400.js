94b0c8bfa504de41ada2f7013eb27db9a87ab309
Unnati Agarwal <111686630+unnati-agarwal-123@users.noreply.github.com>
2023-08-13 14:54:09 +0530

    Add files via upload

---
+++ build/precache-manifest.b6d554575e9ddbcad6a10b0b42548400.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a94859561696c9ca5c3",
+    "url": "./static/css/main.a4baf876.chunk.css"
+  },
+  {
+    "revision": "7a94859561696c9ca5c3",
+    "url": "./static/js/main.f5140a28.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1dbf7aecf120ccf57d37",
+    "url": "./static/css/2.fa62c4c5.chunk.css"
+  },
+  {
+    "revision": "1dbf7aecf120ccf57d37",
+    "url": "./static/js/2.52afe858.chunk.js"
+  },
+  {
+    "revision": "698c0e5381d595693c67d400445f02d2",
+    "url": "./static/media/me.698c0e53.JPG"
+  },
+  {
+    "revision": "0e802582675cfe2870c541cd3408135c",
+    "url": "./static/media/cv.0e802582.pdf"
+  },
+  {
+    "revision": "d7fe0d7393043c583797bcbc89c7bf79",
+    "url": "./static/media/about.d7fe0d73.jpg"
+  },
+  {
+    "revision": "d2a8d03e25c22730d11582e8c34ab428",
+    "url": "./static/media/qs1.d2a8d03e.jpg"
+  },
+  {
+    "revision": "5757d3b2edcf1c588a6e1e0ad1f16775",
+    "url": "./static/media/PR2.5757d3b2.jpg"
+  },
+  {
+    "revision": "fe88efc71deaedaeb9e9dc743e9de658",
+    "url": "./static/media/PR3.fe88efc7.jpg"
+  },
+  {
+    "revision": "e816aa2e3caf63e26bee65c941d3d91c",
+    "url": "./static/media/PR44.e816aa2e.png"
+  },
+  {
+    "revision": "7c3d48104e431cf8afe17fd55e2c7a23",
+    "url": "./static/media/PR5.7c3d4810.jpg"
+  },
+  {
+    "revision": "0e1c4cc666558190612ec4b59f91a427",
+    "url": "./static/media/PR6.0e1c4cc6.png"
+  },
+  {
+    "revision": "5571408166e0d3ecd90d63e0765cba59",
+    "url": "./static/media/PR1.55714081.pdf"
+  },
+  {
+    "revision": "a6589900b95e7124d61cebb697f6d85d",
+    "url": "./static/media/PR2.a6589900.pdf"
+  },
+  {
+    "revision": "78d56cc8b5d12e8f0388aedf2f468c9a",
+    "url": "./static/media/PR3.78d56cc8.pdf"
+  },
+  {
+    "revision": "7d2042ca1134ce589eb7a9b54d91819f",
+    "url": "./static/media/PR4.7d2042ca.pbix"
+  },
+  {
+    "revision": "337353d9b1bcc93559627a24f4eecab2",
+    "url": "./static/media/PR5.337353d9.pdf"
+  },
+  {
+    "revision": "a0bfc053840155f1dc5232f9065656aa",
+    "url": "./static/media/PR6.a0bfc053.pptx"
+  },
+  {
+    "revision": "0ef0689e17efdb4c8f3ac7fa7f8632b0",
+    "url": "./index.html"
+  }
+];
